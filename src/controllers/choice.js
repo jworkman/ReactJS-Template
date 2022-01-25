10793a3399c4f338bc2aa6671cb29190e8dc7297
jworkman <jworkmandevelopment@gmail.com>
2022-01-24 19:15:05 -0500

    Update choice.js

--- src/controllers/choice.js
+++ src/controllers/choice.js
@@ -1,27 +1,27 @@
 const express = require('express')
-const questionCtlr = express.Router()
+const choiceRouter = express.Router()
 const { Choice, Question } = require('../models')
 
-chioceCtlr.get('/', async (req, res) => {
+choiceRouter.get('/', async (req, res) => {
 	const choices = await Choice.findAll({
 		include: Question
 	})
 	res.json(choices)
 })
 
-chioceCtlr.post('/', async (req, res) => {
+choiceRouter.post('/', async (req, res) => {
 	const choice = await Choice.create( req.body )
 	res.json(choice)
 })
 
-chioceCtlr.get('/:id', async (req, res) => {
+choiceRouter.get('/:id', async (req, res) => {
 	const choice = await Choice.findByPk( Number(req.params.id), {
 		include: Question
 	})
 	res.json(choice.Quiz)
 })
 
-chioceCtlr.post('/:id', async (req, res) => {
+choiceRouter.post('/:id', async (req, res) => {
 	var choice = await Choice.update( req.body, {
 	  where: { id: Number(req.params.id) }
 	})
@@ -29,11 +29,11 @@ chioceCtlr.post('/:id', async (req, res) => {
 	res.json(choice)
 })
 
-chioceCtlr.delete('/:id', async (req, res) => {
+choiceRouter.delete('/:id', async (req, res) => {
 	const deleted = await Choice.destroy({
 		where: { id: Number(req.params.id) }
 	})
 	res.json(deleted)
 })
 
-module.exports = chioceCtlr
+module.exports = choiceRouter
